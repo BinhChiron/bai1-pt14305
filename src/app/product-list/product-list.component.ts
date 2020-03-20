7982e274a651a11646a5b5757217a5bd9e81e452
BinhChiron <dangthanhbinh227@gmail.com>
2020-03-20 14:49:41 +0700

    3 files modified, 5 added

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import{product} from '../product';
+import{data} from '../Modtata';
+import { Data } from '@angular/router';
 
 @Component({
   selector: 'app-product-list',
@@ -10,6 +13,17 @@ export class ProductListComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+  products = data;
+
+  selected: product;
+  showDetail(product){
+    this.selected = product;
+  }
+
+  Delete(id){
+      this.products = this.products.filter(x => x.id !== id);
+      console.log(product);
   }
 
 }
